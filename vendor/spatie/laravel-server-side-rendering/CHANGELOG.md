08d06f1ca1ede5106b3c544d47dc8c772e7aacae
Aleksa1996 <aleksa.j.1996@gmail.com>
2018-12-19 22:26:10 +0100

    Uploading vendor

---
+++ vendor/spatie/laravel-server-side-rendering/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+All notable changes to `laravel-server-side-rendering` will be documented in this file
+
+## 1.0.1 - 2018-09-04
+
+- support for Laravel 5.7
+
+## 1.0.0 - 2018-01-01
+
+- initial release
---
+++ vendor/spatie/server-side-rendering/src/Engines/Node.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Spatie\Ssr\Engines;
+
+use Spatie\Ssr\Engine;
+use Spatie\Ssr\Exceptions\EngineError;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+use Swoole\Coroutine as SwooleCo;
+
+class Node implements Engine
+{
+    /** @var string */
+    protected $nodePath;
+
+    /** @var string */
+    protected $tempPath;
+
+    public function __construct(string $nodePath, string $tempPath)
+    {
+        $this->nodePath = $nodePath;
+        $this->tempPath = $tempPath;
+    }
+
+    // public function run(string $script): string
+    // {
+    //     $tempFilePath = $this->createTempFilePath();
+
+    //     file_put_contents($tempFilePath, $script);
+
+    //     $process = new Process("{$this->nodePath} {$tempFilePath}");
+
+    //     try {
+    //         return substr($process->mustRun()->getOutput(), 0, -1);
+    //     } catch (ProcessFailedException $exception) {
+    //         throw EngineError::withException($exception);
+    //     } finally {
+    //         unlink($tempFilePath);
+    //     }
+    // }
+
+    public function run(string $script) : string
+    {
+        $tempFilePath = $this->createTempFilePath();
+        $nodePath = $this->nodePath;
+
+        file_put_contents($tempFilePath, $script);
+
+        // swoole redis co
+        $redisClient = new SwooleCo\Redis();
+        $redisClient->connect('127.0.0.1', 6379);
+
+        $uri = request()->getRequestUri();
+        $html = null;
+        $exists = (bool)$redisClient->exists($uri);
+        $exists = false;
+        if ($exists) {
+            $html = $redisClient->get($uri);
+        } else {
+            $execResult = SwooleCo::exec($nodePath . ' ' . $tempFilePath);
+            $html = $execResult['output'];
+            $redisClient->set($uri, $html);
+        }
+
+        try {
+            return substr($html ?? '', 0, -1);
+        } catch (ProcessFailedException $exception) {
+            throw EngineError::withException($exception);
+        }
+        finally {
+            unlink($tempFilePath);
+        }
+    }
+
+    public function getDispatchHandler() : string
+    {
+        return 'console.log';
+    }
+
+    protected function createTempFilePath() : string
+    {
+        return implode(DIRECTORY_SEPARATOR, [$this->tempPath, md5(intval(microtime(true) * 1000) . random_bytes(5)) . '.js']);
+    }
+}
